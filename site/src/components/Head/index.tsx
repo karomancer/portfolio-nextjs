31e098660e113919afbf7c28b1e7cec799af922f
Karina <karinachowtime@gmail.com>
2023-07-12 22:51:25 -0400

    Add twitter OG meta tags

--- site/src/components/Head/index.tsx
+++ site/src/components/Head/index.tsx
@@ -9,9 +9,11 @@ interface Props {
   children?: JSX.Element | JSX.Element[];
 }
 
-const Head = ({ children, title, description, keywords, ogUrl, ogImage }: Props) => (
+const Head = ({ children, title, description, keywords, ogUrl, ogImage }: Props) => {
+  const editedTitle = `Karina Chow | ${title}`;
+  return (
   <NextJSHead>
-    <title>Karina Chow | {title} </title>
+    <title>{editedTitle}</title>
     <link rel='shortcut icon' href='/images/favicon.ico' />
     <meta name="author" content="Karina Chow" />
     <meta name="description" content={description} />
@@ -20,13 +22,15 @@ const Head = ({ children, title, description, keywords, ogUrl, ogImage }: Props)
       content={`karina chow, karina, chow, portfolio site, portfolio, personal site, personal website, resume, download, linkedin, links, contact, ${keywords?.join(", ")}, ${title}`}
     />
     {/* Regular meta properties */}
-    <meta property="og:title" content={`Karina Chow | ${title}`} key="title" />
+    <meta property="og:title" content={editedTitle} key="title" />
     <meta property="og:image" content={ogImage} />
     <meta property="og:url" content={ogUrl} />
     {/* Twitter properties */}
+    <meta name="twitter:title" content={editedTitle}></meta>
+    <meta name="twitter:image" content={ogImage}></meta>
     <meta name="twitter:card" content={description}></meta>
     <meta name="twitter:creator" content="@karomancer"></meta>
   </NextJSHead>
-);
+);}
 
 export default Head;
