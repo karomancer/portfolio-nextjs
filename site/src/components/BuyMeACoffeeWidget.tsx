6b3eb548b6ff626f3e742d2f16c02b011edf9fe2
Karina <karinachowtime@gmail.com>
2023-09-25 02:03:55 -0400

    Use Script tag and use beforeInteractive strategy

--- site/src/components/BuyMeACoffeeWidget.tsx
+++ site/src/components/BuyMeACoffeeWidget.tsx
@@ -4,9 +4,10 @@ import Script from "next/script";
 // TODO: intersection observer so it doesn't appear until after the fold
 const BuyMeACoffeeWidget = () => (
   <Script
+    src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+    strategy="beforeInteractive"
     data-name="BMC-Widget"
     data-cfasync="false"
-    src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
     data-id="karomancer"
     data-description="Support me on Buy me a coffee!"
     data-message="Hey! If you enjoy what I'm doing and want to support me in my creative journey while I'm in graduate school, consider buying me a tea 💚"
@@ -14,7 +15,7 @@ const BuyMeACoffeeWidget = () => (
     data-position="Right"
     data-x_margin="18"
     data-y_margin="18"
-  ></Script>
+  />
 );
 
 export default BuyMeACoffeeWidget;
