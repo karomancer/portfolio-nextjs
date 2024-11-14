65d9b7d8a0a4ec0654fb9e6bd0bdc242d8f1405b
karomancer <karinachowtime@gmail.com>
2024-11-14 08:27:05 -0800

    Change width on resume page

--- site/src/pages/resume.tsx
+++ site/src/pages/resume.tsx
@@ -25,7 +25,7 @@ const Resume = () => (
       <meta property="og:url" content="http://www.karinachowtime.com/resume" />
     </Head>
     <CircuitsHeader>Let's get professional.</CircuitsHeader>
-    <main>
+    <main className={styles["resume"]}>
       <PDFViewer pdfUrl={RESUME_URL} withBoxShadow />
     </main>
   </>
