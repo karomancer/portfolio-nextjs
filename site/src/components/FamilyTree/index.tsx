79cb8ae794bf0d610f6bbd0efa5a4d7f5c86b356
karomancer <karinachowtime@gmail.com>
2025-08-25 19:40:25 -0700

    Make font larger on family tree

--- site/src/components/FamilyTree/index.tsx
+++ site/src/components/FamilyTree/index.tsx
@@ -1,60 +1,66 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import f3 from "family-chart"; // npm install family-chart@0.7.2 or yarn add family-chart@0.7.2
 
 import FAMILY_DATA from "./family-tree.json";
 
 import "family-chart/styles/family-chart.css";
+import styles from "./styles.module.scss";
 
-export default class FamilyTree extends React.Component {
-  cont = React.createRef();
+function create(data) {
+  const f3Chart = f3
+    .createChart("#FamilyChart", data)
+    .setTransitionTime(1000)
+    .setCardXSpacing(300)
+    .setCardYSpacing(150)
+    .setSingleParentEmptyCard(false, { label: "ADD" })
+    .setShowSiblingsOfMain(false)
+    .setOrientationVertical()
+    .setSortChildrenFunction((a, b) =>
+      a.data["first name"] === b.data["first name"]
+        ? 0
+        : a.data["first name"] > b.data["first name"]
+        ? 1
+        : -1
+    );
 
-  componentDidMount() {
-    if (!this.cont.current) return;
+  const f3Card = f3Chart
+    .setCard(f3.CardHtml)
+    .setCardDisplay([["first name", "last name"], ["birthday"]])
+    .setCardDim({ width: 270, height: 85 })
+    .setMiniTree(true)
+    .setStyle("imageRect")
+    .setOnHoverPathToMain();
 
-    create(FAMILY_DATA);
+  const f3EditTree = f3Chart
+    .editTree()
+    .fixed(true)
+    .setFields(["first name", "last name", "birthday", "avatar"])
+    .setEditFirst(false)
+    .setCardClickOpen(f3Card);
 
-    function create(data) {
-      const f3Chart = f3
-        .createChart("#FamilyChart", data)
-        .setTransitionTime(1000)
-        .setCardXSpacing(250)
-        .setCardYSpacing(150)
-        .setSingleParentEmptyCard(false, { label: "ADD" })
-        .setShowSiblingsOfMain(false)
-        .setSortChildrenFunction((a, b) =>
-          a.data["first name"] === b.data["first name"]
-            ? 0
-            : a.data["first name"] > b.data["first name"]
-            ? 1
-            : -1
-        )
-        .setOrientationVertical();
+  f3EditTree.setNoEdit();
 
-      const f3Card = f3Chart
-        .setCard(f3.CardHtml)
-        .setCardDisplay([["first name", "last name"], ["birthday"]])
-        .setCardDim({ width: 220, height: 85 })
-        .setMiniTree(true)
-        .setStyle("imageRect")
-        .setOnHoverPathToMain();
+  f3Chart.updateTree({ initial: true });
+  //   f3EditTree.open(f3Chart.getMainDatum());
 
-      //   const f3EditTree = f3Chart
-      //     .editTree()
-      //     .fixed(true)
-      //     .setFields(["first name", "last name", "birthday", "avatar"])
-      //     .setEditFirst(true)
-      //     .setCardClickOpen(f3Card);
-
-      //   f3EditTree.setEdit();
-
-      f3Chart.updateTree({ initial: true });
-      //   f3EditTree.open(f3Chart.getMainDatum());
-
-      f3Chart.updateTree({ initial: true });
-    }
-  }
-
-  render() {
-    return <div className="f3 f3-cont" id="FamilyChart" ref={this.cont}></div>;
-  }
+  f3Chart.updateTree({ initial: true });
 }
+
+const FamilyTree = () => {
+  const cont = useRef();
+
+  useEffect(() => {
+    if (!cont.current) return;
+    create(FAMILY_DATA);
+  }, [cont.current]);
+
+  return (
+    <div
+      className={`f3 f3-cont ${styles["family-chart"]}`}
+      id="FamilyChart"
+      ref={cont}
+    ></div>
+  );
+};
+
+export default FamilyTree;
