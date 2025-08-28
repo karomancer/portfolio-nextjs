54530407fb5f8b9f89d67e640f1f419a559e0ba7
karomancer <karinachowtime@gmail.com>
2025-08-28 14:06:54 -0700

    Update family tree for pictures and gender and hierarchy

--- site/src/components/FamilyTree/index.tsx
+++ site/src/components/FamilyTree/index.tsx
@@ -1,6 +1,10 @@
 import React, { useRef, useEffect } from "react";
 import f3 from "family-chart"; // npm install family-chart@0.7.2 or yarn add family-chart@0.7.2
 
+// Next IDs:
+// 03
+// 100
+// 225
 import FAMILY_DATA from "./family-tree.json";
 
 import "family-chart/styles/family-chart.css";
@@ -10,25 +14,18 @@ function create(data) {
   const f3Chart = f3
     .createChart("#FamilyChart", data)
     .setTransitionTime(1000)
-    .setCardXSpacing(300)
-    .setCardYSpacing(150)
+    .setCardXSpacing(375)
+    .setCardYSpacing(400)
     .setSingleParentEmptyCard(false, { label: "ADD" })
     .setShowSiblingsOfMain(false)
-    .setOrientationVertical()
-    .setSortChildrenFunction((a, b) =>
-      a.data["first name"] === b.data["first name"]
-        ? 0
-        : a.data["first name"] > b.data["first name"]
-        ? 1
-        : -1
-    );
+    .setOrientationVertical();
 
   const f3Card = f3Chart
     .setCard(f3.CardHtml)
     .setCardDisplay([["first name", "last name"], ["birthday"]])
-    .setCardDim({ width: 270, height: 85 })
-    .setMiniTree(true)
-    .setStyle("imageRect")
+    .setCardDim({ width: 200, height: 200 })
+    // .setMiniTree(true)
+    .setStyle("imageCircle")
     .setOnHoverPathToMain();
 
   const f3EditTree = f3Chart
